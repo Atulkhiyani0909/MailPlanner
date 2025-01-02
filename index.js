170209daec47d0b0896c0f964d0925863b0dd009
Atul <atulkhiyani09@gmail.com>
2025-01-02 19:48:26 +0530

    Done

--- index.js
+++ index.js
@@ -84,8 +84,8 @@ app.use(express.urlencoded({ extended: true }));
 const sessionOptions={
   store,
   secret:process.env.SECRET,
-  resave:false,
-  saveUnintialized:true,
+  resave: false,            
+    saveUninitialized: true,  
   cookie:{
     expires:Date.now()+7*24*60*60*1000,//in millisecond this is about seven days
     maxAge:7*24*60*60*1000,
